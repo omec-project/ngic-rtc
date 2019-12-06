3a95528b3b7f56d2caba7201285c13d6477a44bc
Ajay Thakur <thakur.ajay@opennetworking.org>
2019-12-06 11:15:18 -0700

    SPGWU should support dynamic config for dp selection #90

--- cp/cp_config.h
+++ cp/cp_config.h
@@ -3,18 +3,6 @@
 *
 * SPDX-License-Identifier: Apache-2.0
 *
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
 */
 #ifndef __CP_CONFIG_H__
 #define __CP_CONFIG_H__
@@ -65,6 +53,10 @@ struct app_config
 
 void init_spgwc_dynamic_config(struct app_config *cfg);
 
+/* Application can pass the dp_key and get back one of the selected DP in return.
+ * Over the period of time dp_key will have multiple keys and this API will 
+ * go through all the config to return one of the first matching DP ID.
+*/
 uint64_t select_dp_for_key(struct dp_key *); 
 
 #endif
